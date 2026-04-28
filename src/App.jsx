eeb4a6f9e21acda6141ced4c19517bb0d3eb4066
ritik kumar <ritikchugh17@gmail.com>
2026-04-28 11:44:50 +0530

    add image from assets and public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Firstcomp, { SecondComp } from "./components/Firstcomp";
+import assetsimage from "./assets/hero.png"
 
 function App() {
   return (
     <>
+    <img src="./public/favicon.svg" />
+    <img src = {assetsimage} />
       <h1>hello word</h1>
      <Firstcomp/>
      <SecondComp/>
+     
     </>
   );
 }
